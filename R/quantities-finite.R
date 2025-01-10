f13a715c86af7e3a7e2c296230480f4ab3b57cc5
Vincenzo Coia <vincenzo.coia@gmail.com>
2025-01-10 12:02:45 -0800

    move empirical distribution in from distionary.

---
+++ R/quantities-finite.R
@@ -0,0 +1,45 @@
+#' @export
+mean.finite <- function(x, ...) {
+	with(x$probabilities, {
+		sum(size * location)
+	})
+}
+
+
+#' @export
+variance.finite <- function(distribution, ...) {
+  mu <- mean(distribution)
+	with(distribution$probabilities, {
+		mu2 <- sum(size * location^2)
+		mu2 - mu^2
+	})
+}
+
+#' @export
+skewness.finite <- function(distribution, ...) {
+	mu <- mean(distribution)
+	sigma <- stdev(distribution)
+	with(distribution$probabilities, {
+		trans <- ((location - mu) / sigma)^3
+		sum(trans * size)
+	})
+}
+
+#' @export
+kurtosis_exc.finite <- function(distribution, ...) {
+	mu <- mean(distribution)
+	sigma <- stdev(distribution)
+	with(distribution$probabilities, {
+		trans <- ((location - mu) / sigma)^4
+		sum(trans * size) - 3
+	})
+}
+
+#' @rdname range
+#' @export
+range.finite <- function(distribution, ...) {
+	unlisted_probability_list <- distribution$probabilities$location
+	min_val <- min(unlisted_probability_list)
+	max_val <- max(unlisted_probability_list)
+	c(min_val, max_val)
+}
